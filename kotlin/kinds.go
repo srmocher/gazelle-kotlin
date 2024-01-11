400134f16c7f1232f196fdc1ac3b8c62b260bcdf
Sridhar Mocherla <sridhar.mocherla@outlook.com>
2024-01-11 20:24:26 +0530

    Add default set of loads

--- kotlin/kinds.go
+++ kotlin/kinds.go
@@ -22,8 +22,26 @@ var kotlinKinds = map[string]rule.KindInfo{
 	},
 }
 
+var kotlinLoads = []rule.LoadInfo{
+	{
+		Name: "@io_bazel_rules_kotlin//kotlin:jvm.bzl",
+		Symbols: []string{
+			"kt_jvm_library",
+			"kt_jvm_test",
+			"kt_jvm_binary",
+		},
+	},
+	{
+		Name: "@com_github_grpc_grpc_kotlin//:kt_jvm_grpc.bzl",
+		Symbols: []string{
+			"kt_jvm_grpc_library",
+			"kt_jvm_proto_library",
+		},
+	},
+}
+
 func (*kotlinLang) Loads() []rule.LoadInfo {
-	return nil
+	return kotlinLoads
 }
 
 func (*kotlinLang) Kinds() map[string]rule.KindInfo {
