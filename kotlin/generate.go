667bd464095c4a77b4b3d64198525b26bc7c5324
Sridhar Mocherla <sridhar.mocherla@outlook.com>
2024-01-12 19:33:45 +0530

    Generate proto/grpc targets

--- kotlin/generate.go
+++ kotlin/generate.go
@@ -3,6 +3,7 @@ package kotlin
 import (
 	"fmt"
 	"github.com/bazelbuild/bazel-gazelle/language"
+	"github.com/bazelbuild/bazel-gazelle/language/proto"
 	"github.com/bazelbuild/bazel-gazelle/rule"
 	"github.com/srmocher/gazelle-kotlin/kotlin/internal"
 	"github.com/srmocher/gazelle-kotlin/kotlin/protobuf"
@@ -31,6 +32,15 @@ func (*kotlinLang) GenerateRules(args language.GenerateArgs) language.GenerateRe
 	c := args.Config
 	kc := GetKotlinConfig(c)
 
+	var protoRules []*rule.Rule
+	var protoImportInfos []*packageImportInfo
+	var err error
+	if kc.ktGenerateProto {
+		protoRules, protoImportInfos, err = genProtoRules(args.Rel, args.OtherGen)
+		if err != nil {
+			log.Print(err)
+		}
+	}
 	if !kc.buildFileGenerationEnabled {
 		// Build file generation is disabled
 		return language.GenerateResult{}
@@ -44,13 +54,17 @@ func (*kotlinLang) GenerateRules(args language.GenerateArgs) language.GenerateRe
 		return language.GenerateResult{}
 	}
 
-	imports := make([]interface{}, 0, len(packageImportInfos))
+	imports := make([]interface{}, 0, len(packageImportInfos)+len(protoImportInfos))
 	for _, pii := range packageImportInfos {
 		imports = append(imports, pii)
 	}
 
+	for _, pii := range protoImportInfos {
+		imports = append(imports, pii)
+	}
+
 	return language.GenerateResult{
-		Gen:     rules,
+		Gen:     append(rules, protoRules...),
 		Imports: imports,
 	}
 }
@@ -83,6 +97,58 @@ func filterImports(sis []*protobuf.SourceFileInfo, mii *internal.MavenInstallInf
 	return externalImports, otherImports
 }
 
+func getJavaPackage(rel string, protoPkg proto.Package) string {
+	var javaPkg string = ""
+	for k, v := range protoPkg.Options {
+		if k == "java_package" {
+			javaPkg = v
+		}
+	}
+
+	if javaPkg == "" {
+		javaPkg = fmt.Sprintf("%s.%s", rel, protoPkg.Name)
+	}
+
+	return javaPkg
+}
+
+func genProtoRules(rel string, otherGen []*rule.Rule) ([]*rule.Rule, []*packageImportInfo, error) {
+	var rules []*rule.Rule
+	var pkgImportInfos []*packageImportInfo
+
+	var protoPkg proto.Package
+	foundProto := false
+
+	for _, r := range otherGen {
+		if r.Kind() == "proto_library" {
+			protoPkg = r.PrivateAttr(proto.PackageKey).(proto.Package)
+			foundProto = true
+			break
+		}
+	}
+	if !foundProto {
+		return []*rule.Rule{}, []*packageImportInfo{}, nil
+	}
+
+	javaPkg := getJavaPackage(rel, protoPkg)
+	if protoPkg.HasServices {
+		grpcRule := rule.NewRule("kt_jvm_grpc_library", "kt_grpc_library")
+		grpcRule.SetAttr("visibility", []string{"//visibility:public"})
+		pkgImportInfos = append(pkgImportInfos, &packageImportInfo{
+			PackageName: javaPkg,
+		})
+		rules = append(rules, grpcRule)
+	}
+
+	protoRule := rule.NewRule("kt_jvm_proto_library", "kt_proto_library")
+	protoRule.SetAttr("visibility", []string{"//visibility:public"})
+	pkgImportInfos = append(pkgImportInfos, &packageImportInfo{
+		PackageName: javaPkg,
+	})
+	rules = append(rules, protoRule)
+	return rules, pkgImportInfos, nil
+}
+
 func buildKotlinPackage(rel string, kc *KotlinConfig, ktFiles []string) ([]*rule.Rule, []*packageImportInfo, error) {
 	sourceFileInfos, err := kc.kotlinParser.ParseKotlinFiles(ktFiles)
 	if err != nil {
@@ -101,6 +167,7 @@ func buildKotlinPackage(rel string, kc *KotlinConfig, ktFiles []string) ([]*rule
 	r := rule.NewRule("kt_jvm_library", defaultKtNamingConvention)
 	sort.Strings(ktFiles)
 	r.SetAttr("srcs", ktFiles)
+	r.SetPrivateAttr("package", sourcePackage)
 
 	if len(kc.ktVisibility) > 0 {
 		r.SetAttr("visibility", kc.ktVisibility)
--- kotlin/resolve.go
+++ kotlin/resolve.go
@@ -1,21 +1,93 @@
 package kotlin
 
 import (
+	"fmt"
 	"github.com/bazelbuild/bazel-gazelle/config"
 	"github.com/bazelbuild/bazel-gazelle/label"
 	"github.com/bazelbuild/bazel-gazelle/repo"
 	"github.com/bazelbuild/bazel-gazelle/resolve"
 	"github.com/bazelbuild/bazel-gazelle/rule"
+	"log"
+	"strings"
 )
 
 func (*kotlinLang) Imports(c *config.Config, r *rule.Rule, f *rule.File) []resolve.ImportSpec {
-	return nil
+	if !isKtJvmLibrary(r.Kind()) || !isKtJvmProtoLibrary(r.Kind()) {
+		return nil
+	}
+
+	pkg := r.PrivateAttr("package")
+	if pkg != nil {
+		pkgStr := pkg.(string)
+		return []resolve.ImportSpec{
+			{
+				Lang: kotlinName,
+				Imp:  pkgStr,
+			},
+		}
+	} else {
+		return []resolve.ImportSpec{}
+	}
+
 }
 
 func (*kotlinLang) Embeds(r *rule.Rule, from label.Label) []label.Label {
 	return nil
 }
 
+func isStandardImport(imp string) bool {
+	return strings.HasPrefix(imp, "kotlin.")
+}
+
 func (*kotlinLang) Resolve(c *config.Config, ix *resolve.RuleIndex, rc *repo.RemoteCache, r *rule.Rule, imports interface{}, from label.Label) {
-	return
+	if !isKtJvmLibrary(r.Kind()) || !isKtJvmProtoLibrary(r.Kind()) {
+		return
+	}
+
+	kc := GetKotlinConfig(c)
+
+	r.DelAttr("deps")
+	pkgImportInfo := imports.(packageImportInfo)
+
+	var deps []string
+	for _, imp := range pkgImportInfo.ExternalImports {
+		deps = append(deps, kc.mavenInstallInfo.CoordToBazelLabel(imp.Coord))
+	}
+
+	for _, imp := range pkgImportInfo.OtherImports {
+		// No need for an explicit dep if it's a standard lib import
+		if isStandardImport(imp) {
+			continue
+		}
+
+		impParts := strings.Split(imp, ".")
+		impPkg := strings.Join(impParts[:len(impParts)-1], ".")
+		l, err := resolveWithIndex(ix, c, impPkg)
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		deps = append(deps, l.String())
+	}
+	r.SetAttr("deps", deps)
+}
+
+func resolveWithIndex(ix *resolve.RuleIndex, c *config.Config, imp string) (label.Label, error) {
+	matches := ix.FindRulesByImportWithConfig(c, resolve.ImportSpec{Lang: kotlinName, Imp: imp}, kotlinName)
+	if len(matches) == 0 {
+		return label.NoLabel, fmt.Errorf("no label found for %s", imp)
+	}
+
+	if len(matches) > 1 {
+		return label.NoLabel, fmt.Errorf("more than 1 match for %s", imp)
+	}
+	return matches[0].Label, nil
+}
+
+func isKtJvmLibrary(kind string) bool {
+	return kind == "kt_jvm_library"
+}
+
+func isKtJvmProtoLibrary(kind string) bool {
+	return kind == "kt_jvm_proto_library" || kind == "kt_jvm_grpc_library"
 }
